d37bd8b6012401e514b5d4e81ce12ad863f5b065
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2018-08-19 05:29:48 -0500

    Create IMGP3.h

---
+++ IMGP3.h
@@ -0,0 +1,64 @@
+#include <SFML/Window/Window.hpp>
+#include <IMGP3>
+#include <SFML/Window/Mouse.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <ctime>
+
+namespace P3;
+class IMGP3
+{
+  public:
+	void P3()
+	{
+		int Import;
+	}
+	IMGP3 P3;
+	P3();
+	P3.Import();
+	{
+		void WindowDisplay();
+		{
+			int Window;
+		Window = Screen_Size(15, 16, 150, 112, 123, 124, 300);
+		
+      Window::IMGP3;
+      Mouse::IMGP3;
+      Mouse = Get_Input();
+      
+  Str_GetInput(Get_Input);
+  Get_Input -> P3;
+
+Get_Input = Mouse_Input(Right_Click);
+      
+      Window -> WindowDisplay();
+      Screen_Size -> WindowDisplay();
+		}
+		Texture::IMGP3
+		{
+			void assign_Texture();
+			{
+				P3 -> assign_Texture();
+					Texture -> assign_Texture();
+					ofstream Texture;
+					Texture.open(".png", ".jpg");
+			assign_Texture();
+			
+			}
+		Animation::Animation
+		time_t time;
+		struct tm * timeinfo;
+		timeinfo = localtime (&time);
+			P3 -> assign_Texture();
+
+    void Move_Model();
+    {
+    	         
+    	       int Range;
+    	Range = Ang;
+    	    Range();
+  Range -> &Move_model;
+        Animation::Range
+       Animation::time
+  clock_time(&time);
+		
+	};
